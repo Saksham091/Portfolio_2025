9616a5e27445801ed1bd05b293614a3419382b6f
Saksham091 <krishnaagrwal4@gmail.com>
2025-08-17 23:55:20 +0530

    Removed unnecessary comments

--- src/section/TechStack.jsx
+++ src/section/TechStack.jsx
@@ -34,20 +34,6 @@ const TechStack = () => {
                     </div>
                 ))}
 
-                {/* {techStackImgs.map((icon) => (
-                    <div key={icon.name} className="card-border tech-card overflow-hidden group xl:rounded-full rounded-lg">
-                        <div className="tech-card-animated-bg" />
-                        <div className="tech-card-content">
-                            <div className="tech-icon-wrapper">
-                                <img src={icon.imgPath} />
-                            </div>
-                            <div className="padding-x w-full">
-                                <p>{icon.name}</p>
-                            </div>
-                        </div>
-                    </div>
-                ))} */}
-
             </div>
 
         </div>
